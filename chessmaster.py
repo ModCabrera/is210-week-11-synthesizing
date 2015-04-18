962d6686a0826aa50ccefbc416bbaa85bf95792e
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-04-18 05:04:16 +0000

    question

--- chessmaster.py
+++ chessmaster.py
@@ -2,55 +2,75 @@
 # -*- coding: utf-8 -*-
 """Chessmaster Piece Tracker"""
 import time
-
+import pprint
 
 class ChessPiece(object):
     prefix = ''
 
     def __init__(self, position):
-        self.position = position
-        self.moves = []
+        if not self.is_legal_move(position):
+            excep = '`{}` is not a legal start position'
+            raise ValueError(excep.format(position))
+        else:
+            self.position = position
+            self.moves = []
+            self.holdposition = position
 
     def algebraic_to_numeric(self, tile):
-        axis_x = {0: 'a',
-                  1: 'b',
-                  2: 'c',
-                  3: 'd',
-                  4: 'e',
-                  5: 'f',
-                  6: 'g',
-                  7: 'h'
+        axis_x = {'a': 0,
+                  'b': 1,
+                  'c': 2,
+                  'd': 3,
+                  'e': 4,
+                  'f': 5,
+                  'g': 6,
+                  'h': 7
                   }
-        axis_y = {0: '1',
-                  1: '2',
-                  2: '3',
-                  3: '4',
-                  4: '5',
-                  5: '6',
-                  6: '7',
-                  7: '8'
+        axis_y = {'1': 0,
+                  '2': 1,
+                  '3': 2,
+                  '4': 3,
+                  '5': 4,
+                  '6': 5,
+                  '7': 6,
+                  '8': 7
                   }
-        
+    
         tilelist = list(tile)
-        self.newtile = {}
-        keyindex = 0 
-        for xkey, xvalue in axis_x.iteritems():
-            if tilelist[0] == xvalue:
-                keyindex = xkey
-                self.newtile.update({xkey:0})
-        for ykey, yvalue in axis_y.iteritems():
-            if tilelist[1] == yvalue:
-                self.newtile[keyindex] = ykey
-                self.newtile = self.newtile.items().pop()
-        if sum(self.newtile) >= 15:
-            self.newtile = None
-            return self.newtile
+        newtile = []
+        for item in tile:
+            if item in axis_x:
+                newtile.append(axis_x[item])
+            elif item in axis_y:
+                newtile.append(axis_y[item])
+                newtile = tuple(newtile)
+                self.position = newtile
+            else:
+                return None
+        return newtile
+
+    def is_legal_move(self, position):
+        newposition = self.algebraic_to_numeric(position)
+        if newposition is None:
+            return False
         else:
-            return self.newtile
+            return True
+
+    def move(self, position):
+        newposition = self.prefix + position
+        if self.is_legal_move(position):
+            oldposition = self.prefix + self.holdposition
+            newmove = []
+            newmove.append(oldposition)
+            newmove.append(newposition)
+            newmove.append(time.time())
+            newmove = tuple(newmove)
+            self.moves.append(newmove)
+            return self.moves
         
+    
+
+
 if __name__ == '__main__':
-    piece = ChessPiece('g8')
-    print piece.position
-    print piece.algebraic_to_numeric('a2')
-    print piece.algebraic_to_numeric('g8')
-    print piece.algebraic_to_numeric('j9')
+    piece = ChessPiece('a1')
+    print piece.move('c4')
